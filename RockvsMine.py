800413755d8a1a4d3f9ea443330bc73c2b93b5d0
sthaarekh <arekhshrestha@gmail.com>
2025-06-22 22:52:05 +0545

    separated data and labels

--- RockvsMine.py
+++ RockvsMine.py
@@ -5,4 +5,10 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
 sonar_data = pd.read_csv('/Users/sthaarekh/Documents/ /                     /Python/sonar.csv', header=None)
-print(sonar_data.describe())
+
+print(sonar_data.describe())  # gives the overview of the data
+
+#separating data and labels
+x = sonar_data.drop(columns=60, axis=1)
+y = sonar_data[60]  #--> stores the data of rock and mine
+print(x,y)
