87892185707e41d3eaa335e6da702e35f5459871
sthaarekh <arekhshrestha@gmail.com>
2025-06-22 22:54:32 +0545

    Splitted the training and test data

--- RockvsMine.py
+++ RockvsMine.py
@@ -12,3 +12,7 @@ print(sonar_data.describe())  # gives the overview of the data
 x = sonar_data.drop(columns=60, axis=1)
 y = sonar_data[60]  #--> stores the data of rock and mine
 print(x,y)
+
+#splitting training and testing data
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, stratify=y, random_state=1)
+print(x.shape, x_train.shape, x_test.shape)
