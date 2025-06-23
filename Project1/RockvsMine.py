6bf8b081f43d59fcba723ea822928565a3db503d
sthaarekh <arekhshrestha@gmail.com>
2025-06-23 08:59:55 +0545

    Project1: Rock vs Mine completed

--- Project1/RockvsMine.py
+++ Project1/RockvsMine.py
@@ -4,14 +4,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-sonar_data = pd.read_csv('/Users/sthaarekh/Documents/ /                     /Python/sonar.csv', header=None)
+sonar_data = pd.read_csv('/Users/sthaarekh/Documents/ /                     /Python/Project1/sonar.csv', header=None)
 
 print(sonar_data.describe())  # gives the overview of the data
 
 #separating data and labels
 x = sonar_data.drop(columns=60, axis=1)
 y = sonar_data[60]  #--> stores the data of rock and mine
-print(x,y)
+# print(x,y)
 
 #splitting training and testing data
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, stratify=y, random_state=1)
