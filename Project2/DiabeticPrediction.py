3b63ac91e47933fd51a6e308af174c4d05bc513f
sthaarekh <arekhshrestha@gmail.com>
2025-06-23 09:49:02 +0545

    Imported packages

---
+++ Project2/DiabeticPrediction.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
