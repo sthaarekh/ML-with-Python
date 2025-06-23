7ffe0ff3cc00a55910732bf4fa5fde6013096356
sthaarekh <arekhshrestha@gmail.com>
2025-06-23 09:53:25 +0545

    dataset imported

--- Project2/DiabeticPrediction.py
+++ Project2/DiabeticPrediction.py
@@ -5,3 +5,8 @@ from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 
+#importing dataset
+diabetes_data = pd.read_csv('/Users/sthaarekh/Documents/ /                     /Python/Project2/diabetes.csv')
+print(diabetes_data.shape)
+print(diabetes_data.describe())
+print(diabetes_data['Outcome'].value_counts())
