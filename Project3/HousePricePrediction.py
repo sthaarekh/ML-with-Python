84c35e36e7eacc37c3d08973c3b7086d6b63ce82
sthaarekh <arekhshrestha@gmail.com>
2025-06-24 15:20:44 +0545

    dataset imported and loaded to pandas

--- Project3/HousePricePrediction.py
+++ Project3/HousePricePrediction.py
@@ -6,3 +6,14 @@ from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
 from sklearn import metrics
+
+
+#importing the dataset of california
+house_dataset = fetch_california_housing()
+print(house_dataset)
+
+#loading the dataset into pandas
+house_dataframe = pd.DataFrame(house_dataset.data, columns = house_dataset.feature_names)
+house_dataframe['price'] = house_dataset.target
+print(house_dataframe)
+
