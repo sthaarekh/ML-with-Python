74237e1e0983fabe7f6a6128a87f08035a9233e2
sthaarekh <arekhshrestha@gmail.com>
2025-06-24 15:25:34 +0545

    understanding the correlation between features using heatmap

--- Project3/HousePricePrediction.py
+++ Project3/HousePricePrediction.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
@@ -17,3 +17,10 @@ house_dataframe = pd.DataFrame(house_dataset.data, columns = house_dataset.featu
 house_dataframe['price'] = house_dataset.target
 print(house_dataframe)
 
+#understanding the correlation between features
+correlation = house_dataframe.corr()
+
+#constructing a heatmap
+plt.figure(figsize = (10,10))
+sns.heatmap(correlation, cbar=True, square=True, fmt='.2f', annot=True, annot_kws={'size':8}, cmap='Blues')
+plt.show()
