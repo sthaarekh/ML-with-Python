69be8bb0a86449abe045b9ed24f11d3be028313b
sthaarekh <arekhshrestha@gmail.com>
2025-06-24 18:08:23 +0545

    Fake news started and imported dependencies

---
+++ Project4/FakeNewsPrediction.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import re
+from nltk.corpus import stopwords   #nltk---> natural language tool kit
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score
