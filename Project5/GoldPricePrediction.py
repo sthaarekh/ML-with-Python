bf28651d204ee1cb7036bafe39954ae389fdd22f
sthaarekh <arekhshrestha@gmail.com>
2025-06-24 23:02:57 +0545

    Gold data loaded

--- Project5/GoldPricePrediction.py
+++ Project5/GoldPricePrediction.py
@@ -5,3 +5,7 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import metrics
+
+#loading the gold data
+gold_data = pd.read_csv('/Users/sthaarekh/Documents/ /                     /Python/Project5/gold_model_dataset_2015_2025.csv')
+gold_data = gold_data.drop(index=gold_data.index[0])
