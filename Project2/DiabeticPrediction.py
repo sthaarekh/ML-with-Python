3d558e957a9d0f31c16e9b4acd07ec6523e55a15
sthaarekh <arekhshrestha@gmail.com>
2025-06-24 13:50:46 +0545

    Traing and testing the model

--- Project2/DiabeticPrediction.py
+++ Project2/DiabeticPrediction.py
@@ -24,3 +24,17 @@ x = standerized_data
 
 #traning and testing data
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, stratify=y, random_state=2)
+
+#training the model
+classifier = svm.SVC(kernel = 'linear')
+classifier.fit(x_train, y_train)
+
+#training data accuracy
+x_train_prediction = classifier.predict(x_train)
+accuracy_train = accuracy_score(x_train_prediction, y_train)
+print("Training accuracy score is", accuracy_train)
+
+#testing data accuracy
+x_test_prediction = classifier.predict(x_test)
+accuracy_test = accuracy_score(x_test_prediction, y_test)
+print("Testing accuracy score is", accuracy_test)
