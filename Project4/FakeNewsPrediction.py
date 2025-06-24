b489ef29525eebcd76c6d2cc701ffc96694bd6dc
sthaarekh <arekhshrestha@gmail.com>
2025-06-24 18:17:41 +0545

    Data preprocessing done

--- Project4/FakeNewsPrediction.py
+++ Project4/FakeNewsPrediction.py
@@ -7,3 +7,17 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import accuracy_score
+import nltk
+nltk.download('stopwords')
+
+# print(stopwords.words('english'))     #printing the stopwords
+
+#data preprocessing
+news_dataset = pd.read_csv('/Users/sthaarekh/Documents/ /                     /Python/Project4/train.csv')
+news_dataset.shape
+news_dataset.head()
+news_dataset.isnull().sum()
+news_dataset = news_dataset.fillna('')      #replacing the null values with null string
+
+
+
