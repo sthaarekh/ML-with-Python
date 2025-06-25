783374c5ccc382e18e0188d1050d4fd14640ec33
sthaarekh <arekhshrestha@gmail.com>
2025-06-25 22:33:45 +0545

    Imported the dependencies of project7

---
+++ Project7/SpamMail.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction import TfidVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+
