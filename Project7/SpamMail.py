5dc0de1129071a6e26d3e3d4632ac96ee1b2f46d
sthaarekh <arekhshrestha@gmail.com>
2025-06-25 23:13:21 +0545

    Data imported and preprocessing done

--- Project7/SpamMail.py
+++ Project7/SpamMail.py
@@ -5,4 +5,10 @@ from sklearn.feature_extraction import TfidVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
+#data collection and preprocessing
+mail_data = pd.read_csv('/Users/sthaarekh/Documents/ /                     /Python/Project7/mail_data.csv')
+print(mail_data.tail())
 
+# label spam as 0 and ham as 1
+mail_data.loc[mail_data['Category']== 'spam', 'Category',] = 0
+mail_data.loc[mail_data['Category']== 'ham', 'Category',] = 1
