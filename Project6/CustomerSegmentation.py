4b381ddb1c467a0a84039041fb021eaf35fe93da
sthaarekh <arekhshrestha@gmail.com>
2025-06-25 14:45:00 +0545

    finding the no of clusters using graph

--- Project6/CustomerSegmentation.py
+++ Project6/CustomerSegmentation.py
@@ -3,3 +3,30 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+
+customer_data = pd.read_csv('/Users/sthaarekh/Documents/ /                     /Python/Project6/Mall_Customers.csv')
+print(customer_data.isnull().sum())
+
+#choosing the annual income and spending score
+x = customer_data.iloc[:,[3,4]].values
+print(x)
+
+#choosing the number of clusters
+# WCSS --> within cluster sum of squares
+
+#finding wcss value for different number of clusters
+wcss = []
+for i in range (1,11):
+    kmeans = KMeans(n_clusters=i, init='k-means++', random_state=42)
+    kmeans.fit(x)
+    wcss.append(kmeans.inertia_)
+
+#plot an elbow graph
+sns.set()
+plt.plot(range(1,11), wcss)
+plt.title("The elbow point graph")
+plt.xlabel("No of clusters")
+plt.ylabel("WCSS")
+plt.show()
+
+#from the graph the optimum no of cluster is 5
