2c0fd20dca038dc65fcd55a905942f250e4e9121
sthaarekh <arekhshrestha@gmail.com>
2025-06-26 09:26:04 +0545

    data collection and preprocessing done

--- Project8/MovieReccomendation.py
+++ Project8/MovieReccomendation.py
@@ -4,3 +4,16 @@ import difflib
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+#data collection and preprocessing
+movies_data = pd.read_csv('/Users/sthaarekh/Documents/ /                     /Python/Project8/movies.csv')
+print(movies_data.head())
+print(movies_data.isnull().sum())
+
+#selecting the relevant features
+selected_features = ['genres', 'keywords', 'tagline', 'cast', 'director']
+
+#replacing the missing values
+for feature in selected_features:
+    movies_data[feature] = movies_data[feature].fillna('')
+
+combined_features = movies_data['genres'] + ' ' + movies_data['keywords'] + ' ' + movies_data['tagline']+ ' ' + movies_data['cast']+ ' ' + movies_data['director']
