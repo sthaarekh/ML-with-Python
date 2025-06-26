e63f437e4c910f395d3b68a74ff36bef62e01de7
sthaarekh <arekhshrestha@gmail.com>
2025-06-26 10:42:30 +0545

    listing the similar movies

--- Project8/MovieReccomendation.py
+++ Project8/MovieReccomendation.py
@@ -6,17 +6,17 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 #data collection and preprocessing
 movies_data = pd.read_csv('/Users/sthaarekh/Documents/ /                     /Python/Project8/movies.csv')
-print(movies_data.head())
-print(movies_data.isnull().sum())
+# print(movies_data.head())
+# print(movies_data.isnull().sum())
 
 #selecting the relevant features
-selected_features = ['genres', 'keywords', 'tagline', 'cast', 'director']
+selected_features = ['genres', 'keywords', 'overview', 'tagline', 'cast', 'director']
 
 #replacing the missing values
 for feature in selected_features:
     movies_data[feature] = movies_data[feature].fillna('')
 
-combined_features = movies_data['genres'] + ' ' + movies_data['keywords'] + ' ' + movies_data['tagline']+ ' ' + movies_data['cast']+ ' ' + movies_data['director']
+combined_features = movies_data['genres'] + ' ' + movies_data['keywords'] +  movies_data['overview'] +' ' + movies_data['tagline']+ ' ' + movies_data['cast']+ ' ' + movies_data['director']
 
 #converting the text data to feature vectors
 vectorizer = TfidfVectorizer()
@@ -44,3 +44,16 @@ print(index)
 #getting a list of similar movies
 similarity_score = list(enumerate(similarity[index]))
 print(similarity_score)
+
+#soritng the movies from highest similarity score
+sorted_list = sorted(similarity_score, key= lambda x:x[1], reverse=True)
+print(sorted_list)
+
+#printing the similar movies
+i = 1
+for movies in sorted_list:
+    index = movies[0]
+    title_of_movie = movies_data[movies_data.index == index]['title'].values[0]
+    if(i<=20):
+        print(i,". ",title_of_movie)
+        i+=1
